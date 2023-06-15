dba3434a52492a97afef7bbc26b6e7149e27b4ed
atulkhot <atul.khot@gmail.com>
2023-06-15 15:40:39 +0530

    check_sorted(...) function is implemented

--- BubbleSort.go
+++ BubbleSort.go
@@ -33,7 +33,29 @@ func print_array(arr []int, num_items int) {
 	}
 }
 
+func check_sorted_if_sorted(arr []int) bool {
+	prev := 0
+	for i, v := range arr {
+		if i == 0 {
+			prev = v
+		} else {
+			if prev > v {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func check_sorted(arr []int) {
+	if check_sorted_if_sorted(arr) {
+		fmt.Println("The array is sorted")
+	} else {
+		fmt.Println("The array is NOT sorted!")
+	}
+}
+
 func main() {
-	arr := []int{10, 20, 30}
-	print_array(arr, 2)
+	arr := []int{10, 20, 5, 30}
+	check_sorted(arr)
 }
